be7ad2708b935dae940c7e52c6faa24687a2c0f7
Erick Barcasnegras <erick.barcasnegras@gmail.com>
2020-02-16 18:01:37 -0500

    feat: Enter one valid roman numeral in an input field

---
+++ app.js
@@ -0,0 +1,46 @@
+let input = document.querySelector('input[type="text"]');
+const romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+const decimalNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+input.addEventListener('keyup', event => {
+    event.preventDefault();
+
+    if (event.code === 'Enter') {
+        if (isRoman(input.value.trim().toUpperCase())) {
+            alert("Valid roman numeral!");
+        } else {
+            alert("Invalid roman numeral!");
+        }
+    }
+});
+
+function isRoman(roman) {
+    return hasRomansCharacters(roman) && hasRightRomanPatterns(roman);
+}
+
+function hasRomansCharacters(roman) {
+    roman = roman.split('');
+
+    for (let i=0; i<roman.length; i++) {
+        if (!romanNumerals.includes(roman[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function hasRightRomanPatterns(roman) {
+    if (
+      roman.includes("IIII") ||
+      roman.includes("VV") ||
+      roman.includes("XXXX") ||
+      roman.includes("LL") ||
+      roman.includes("CCCC") ||
+      roman.includes("DD") ||
+      roman.includes("MMMM")
+    )
+      return false;
+
+    return true;
+}
